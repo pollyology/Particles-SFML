93411d4841e51540d982666723d1ae69a8e7ddad
pollyology <pcagaanan01@gmail.com>
2025-05-06 14:50:21 -0700

    Update Matrices.cpp

--- code/Matrices.cpp
+++ code/Matrices.cpp
@@ -1,1 +1,106 @@
+#include "Matrices.h"
 
+namespace Matrices
+{
+    Matrix::Matrix(int _rows, int _cols)
+    {
+        rows = _rows;
+        cols = _cols;
+        a.resize(rows);
+        for (int i = 0; i < rows; i++)
+        {
+            a[i].resize(cols, 0);
+        }
+    }
+
+	// Matrix addition
+	// c = a + b
+    Matrix operator+(const Matrix& a, const Matrix& b)
+    {
+        if (a.getRows() != b.getRows() || a.getCols() != b.getCols())
+        {
+            throw runtime_error("Error: dimensions must agree");
+        }
+
+        Matrix c(a.getRows(), a.getCols());
+
+        for (int i = 0; i < a.getRows(); i++)
+        {
+            for (int j = 0; j < a.getCols(); j++)
+            {
+                c(i, j) = a(i, j) + b(i, j);
+            }
+        }
+        return c;
+    }
+
+	// Matrix multiplication
+	// c = a * b 
+    Matrix operator*(const Matrix& a, const Matrix& b)
+    {
+        if (a.getCols() != b.getRows())
+        {
+            throw runtime_error("Error: dimensions must agree");
+        }
+     
+        Matrix c(a.getRows(), b.getCols());
+
+        // Use triple for loop (i, j, k) for equation: C[i][j] = A[i][k] * B[k][j]
+        // i = rows of A; j = cols of B; k = cols of A / rows of B
+
+        for (int i = 0; i < a.getRows(); i++)
+        {
+            for (int j = 0; j < b.getCols(); j++)
+            {
+                for (int k = 0; k < a.getCols(); k++)
+                {
+                    c(i, j) += a(i, k) * b(k, j);
+                }
+            }
+        }
+        return c;
+    }
+
+	// Matrix comparison (equality)
+	// a == b
+    bool operator==(const Matrix& a, const Matrix& b)
+    {
+        if (a.getRows() != b.getRows() || a.getCols() != b.getCols())
+        {
+            return false;
+        }
+
+        for (int i = 0; i < a.getRows(); i++)
+        {
+            for (int j = 0; j < b.getCols(); j++)
+            {
+                if (abs(a(i, j) - b(i, j)) > 0.001)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+	// Matrix comparison (inequality)
+	// a != b
+    bool operator!=(const Matrix& a, const Matrix& b)
+    {
+        return !(a == b);
+    }
+
+	// Output matrix: separate columns by ' ' and rows by '\n'
+    ostream& operator<<(ostream& os, const Matrices::Matrix& a)
+    {
+        for (int i = 0; i < a.getRows(); i++)
+        {
+            for (int j = 0; j < a.getCols(); j++)
+            {
+                os << setw(10) << a(i, j) << " ";
+            }
+            os << endl;
+        }
+        return os;
+    }
+}
